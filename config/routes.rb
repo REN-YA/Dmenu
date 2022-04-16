f0dff67ef7988bf3b7ecfbe870961000be56fc63
REN-YA <re_n_ya@icloud.com>
2022-04-16 08:18:05 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'homes#top' #TOPページ
+  namespace :store do
+    resources :stores, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :drinks, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :genres, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :categories, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :employees, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :staffs, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_073254) do
+ActiveRecord::Schema.define(version: 2022_04_16_080833) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "store_id"
@@ -60,9 +60,10 @@ ActiveRecord::Schema.define(version: 2022_04_15_073254) do
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "encrypted_password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
   end
 
 end
